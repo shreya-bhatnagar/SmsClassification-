26a9e6026f49d16df2906958d5aa01a8c39042cb
Shreya-PC <shreya@shreya.bhatnagar.knp@gmail.com>
2017-09-18 03:35:55 +0530

    NaiveBayesExample.java changes

--- SmsClassification/src/Input/NaiveBayesExample.java
+++ SmsClassification/src/Input/NaiveBayesExample.java
@@ -11,10 +11,13 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 
 /**
  *
@@ -47,7 +50,6 @@ public class NaiveBayesExample {
     }
     
     
-    
     /**
      * Main method
      * 
@@ -55,46 +57,73 @@ public class NaiveBayesExample {
      * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
+       //ist<String> arguments = runtimeMxBean.getInputArguments();
+       
         //map of dataset files
-        Map<String, URL> trainingFiles = new HashMap<>();
-        trainingFiles.put("Trans", NaiveBayesExample.class.getResource("/datasets/Transactional.txt"));
-        trainingFiles.put("Promo", NaiveBayesExample.class.getResource("/datasets/Promotional.txt"));
-        trainingFiles.put("OTP", NaiveBayesExample.class.getResource("/datasets/OTP.txt"));
-        
-        //loading examples in memory
-        Map<String, String[]> trainingExamples = new HashMap<>();
+        Map<String, URL> trainingFiles=null;
+        NaiveBayes nb = new NaiveBayes();
+        Map<String, String[]> trainingExamples = new HashMap<>();  
+     for(int i=1;i<=3;i++){
+            switch (i) {
+                case 1:
+            trainingFiles = new HashMap<>();
+            trainingFiles.put("Trans", NaiveBayesExample.class.getResource("/datasets/training_data_trans.txt"));
+                    break;
+                case 2:
+            trainingFiles = new HashMap<>();
+            trainingFiles.put("Promo", NaiveBayesExample.class.getResource("/datasets/training_data_promo.txt")); 
+                    break; 
+                default:
+            trainingFiles = new HashMap<>();
+            trainingFiles.put("OTP", NaiveBayesExample.class.getResource("/datasets/training_data_otp.txt"));     
+                    break;
+            }
+      
+         //loading examples in memory
+       
         for(Map.Entry<String, URL> entry : trainingFiles.entrySet()) {
             trainingExamples.put(entry.getKey(), readLines(entry.getValue()));
-        }
-        
+        } 
+        trainingFiles=null;
         //train classifier
-        NaiveBayes nb = new NaiveBayes();
         nb.setChisquareCriticalValue(6.63); //0.01 pvalue
+        System.gc();
         nb.train(trainingExamples);
-        
-        
+     }
         //get trained classifier knowledgeBase
         NaiveBayesKnowledgeBase knowledgeBase = nb.getKnowledgeBase();
         
         nb = null; 
         trainingExamples = null;
         
-        
         //Use classifier and prediction
         nb = new NaiveBayes(knowledgeBase);
+          String DIRECTORY_PATH = "/home/shreya/NetBeansProjects/SmsClassification/src/datasets/";
+         List<String> test_data_lines = Files.readAllLines(Paths.get(DIRECTORY_PATH + "test_data.txt"));
+	 System.out.println(test_data_lines.size() + " lines read from " + Paths.get(DIRECTORY_PATH+ "trans.txt") );
+
+        for (int index = 0; index < 10; index++) {
+         
+         String incomingSMS = test_data_lines.get(index);
+         
+         String predicted_class = nb.predict(incomingSMS);
+         System.out.format("The sentense id="+index+" was classified as:", predicted_class);
+//        
+        }
         
-        String exampleEn = "Rs.2000.00 was withdrawn using your HDFC Bank Card ending 6476 on 2017-03-08:19:55:45 at +SITE NO 650 11TH MAIN. Avl bal: Rs.34654.23";
         
-        String outputEn = nb.predict(exampleEn);
-        System.out.format("The sentense \"%s\" was classified as \"%s\".%n", exampleEn, outputEn);
-        
-        String exampleFr = "Do not miss the special IPL offer! Get extra 10% off @ Biryani Day, Imperio Restaurant, Jaffas Biryani, Delight Food & more. Use code FPWD goo.gl/mWgvjP *T&C.";
-        String outputFr = nb.predict(exampleFr);
-        System.out.format("The sentense \"%s\" was classified as \"%s\".%n", exampleFr, outputFr);
-        
-        String exampleDe = "OTP is 647595 for txn of INR 1893.49 at Myntra Designs Private Li on card ending 8877. Valid till 14:43:44. Do not share OTP for security";
-        String outputDe = nb.predict(exampleDe);
-        System.out.format("The sentense \"%s\" was classified as \"%s\".%n", exampleDe, outputDe);
-  
+//        String exampleEn = "Rs.2000.00 was withdrawn using your HDFC Bank Card ending 6476 on 2017-03-08:19:55:45 at +SITE NO 650 11TH MAIN. Avl bal: Rs.34654.23";
+//        
+//        String outputEn = nb.predict(exampleEn);
+//        System.out.format("The sentense \"%s\" was classified as \"%s\".%n", exampleEn, outputEn);
+//        
+//        String exampleFr = "Do not miss the special IPL offer! Get extra 10% off @ Biryani Day, Imperio Restaurant, Jaffas Biryani, Delight Food & more. Use code FPWD goo.gl/mWgvjP *T&C.";
+//        String outputFr = nb.predict(exampleFr);
+//        System.out.format("The sentense \"%s\" was classified as \"%s\".%n", exampleFr, outputFr);
+//        
+//        String exampleDe = "OTP is 647595 for txn of INR 1893.49 at Myntra Designs Private Li on card ending 8877. Valid till 14:43:44. Do not share OTP for security";
+//        String outputDe = nb.predict(exampleDe);
+//        System.out.format("The sentense \"%s\" was classified as \"%s\".%n", exampleDe, outputDe);
+//  
     }
 }
